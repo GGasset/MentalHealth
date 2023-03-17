c9a57da08f135f1f452567103811056e722f2121
Germán Gasset <gassetgerman@gmail.com>
2023-03-17 11:17:26 +0100

    Almost finished main Finished select_survey() Finished select_question()

--- explore.py
+++ explore.py
@@ -1,5 +1,7 @@
 from sqlite3 import connect, Cursor
 
+from util import *
+
 db_path = './mental_health.sqlite'
 '''
 If you run .schema on the database the result will be:
@@ -22,19 +24,57 @@ CREATE TABLE sqlite_sequence(name,seq);
 '''
 
 def main():
-        mental_health_connection = connect(db_path)
-        mental_health = mental_health_connection.cursor()
-        print('Set-up completed.')
+    mental_health_connection = connect(db_path)
+    mental_health = mental_health_connection.cursor()
+    print('Set-up completed.')
 
-        try:
-                pass
-        finally:
-                mental_health_connection.close()
-                print('Connection closed.')
+    try:
+        survey_id = select_survey(mental_health)
+        question_id = select_question(mental_health)
+    finally:
+        mental_health_connection.close()
+        print('Connection closed.')
 
-def select_survey(cursor: Cursor):
-        survey_years = []
+def select_survey(cursor: Cursor) -> int:
+    survey_years = []
+    cursor.execute('SELECT Description FROM Survey')
+    returned = cursor.fetchall()
+    print('Naming surveys...')
+    for description in returned:
+        description = description[0]
+        splitted = description.split(' ')
+        survey_years.append(int(splitted[-1]))
+        print(description)
 
+    while True:
+        selected_survey_year = GetInputInt('Select a year from the list to select a survey to investigate.')
+        if selected_survey_year in survey_years:
+            cursor.execute(f'SELECT SurveyId, Description FROM Survey WHERE Description LIKE "%{selected_survey_year}"')
+            returned = cursor.fetchall()
+            print('Selected ' + returned[0][1])
+            return returned[0][0]
+        
+        print('The selected year doesn\'t exists in the data.')
+                
+def select_question(cursor: Cursor) -> int:
+    ids = []
+    cursor.execute('SELECT questionid, questiontext FROM Question')
+    results = cursor.fetchall()
+    print('Naming questions...')
+    for result in results:
+        question_id = result[0]
+        ids.append(question_id)
+        print(f'{question_id}. {result[1]}')
+    while True:
+        selected_id = GetInputInt('Select a question id.')
+        if selected_id not in ids:
+            print('Id doesn\'t exists.')
+            continue
+
+        cursor.execute(f'SELECT questiontext FROM Question WHERE questionid = {selected_id}')
+        result = cursor.fetchall()
+        print('Selected "' + result[0][0] + '"')
+        return selected_id
 
 if __name__ == '__main__':
-        main()
+    main()
