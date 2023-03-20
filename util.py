61b74e15652bfbe6afb8cb538e5b663a73d9d06e
Germán Gasset <gassetgerman@gmail.com>
2023-03-20 16:07:04 +0100

    fixed warning

--- util.py
+++ util.py
@@ -12,7 +12,7 @@ def display_answers(question_id: int, question_text: str, answers: list[str]) ->
     reminder = f'{question_id}. {question_text}\n'
     reminder += 'Type "cancel" to terminate the program.'
     for i, answer in enumerate(answers):
-        if i % 27 is 0:
+        if i % 27 == 0:
             print('\n\n', reminder, end='\n\n')
         print(f'Answer {i}: {answer}', end=' ')
         if input().lower() == 'cancel':
